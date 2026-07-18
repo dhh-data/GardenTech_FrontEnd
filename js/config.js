8dbb942b921828a83a4097aa88c079fe7675b8c1
dhh-data <dzakyhilmy01@gmail.com>
2026-07-18 13:32:13 +0700

    fix error

--- js/config.js
+++ js/config.js
@@ -1,23 +1,23 @@
 const APP_CONFIG = {
   API_BASE_URL: "https://gardentechbackend-production.up.railway.app/api/v1",
 
-  AUTH_LOGIN_ENDPOINT: "/auth/login/",
-  AUTH_REGISTER_ENDPOINT: "/auth/register/",
+  AUTH_LOGIN_ENDPOINT: "/auth/login",
+  AUTH_REGISTER_ENDPOINT: "/auth/register",
 
-  USERS_ENDPOINT: "/users/",
-  USER_ROLE_ENDPOINT: "/users/{username}/role/",
-  USER_DELETE_ENDPOINT: "/users/{username}/",
+  USERS_ENDPOINT: "/users",
+  USER_ROLE_ENDPOINT: "/users/{username}/role",
+  USER_DELETE_ENDPOINT: "/users/{username}",
 
-  SENSORS_LATEST_ENDPOINT: "/sensors/latest/",
-  SENSORS_LIST_ENDPOINT: "/sensors/list/",
-  SENSORS_MOISTURE_HISTORY_ENDPOINT: "/sensors/history/moisture/",
-  SENSORS_VOLTAGE_HISTORY_ENDPOINT: "/sensors/history/voltage-current/",
-  DEVICES_ENDPOINT: "/devices/",
-  ACTUATOR_COMMAND_ENDPOINT: "/devices/{deviceId}/command/",
-  LOGS_ENDPOINT: "/logs/",
+  SENSORS_LATEST_ENDPOINT: "/sensors/latest",
+  SENSORS_LIST_ENDPOINT: "/sensors/list",
+  SENSORS_MOISTURE_HISTORY_ENDPOINT: "/sensors/history/moisture",
+  SENSORS_VOLTAGE_HISTORY_ENDPOINT: "/sensors/history/voltage-current",
+  DEVICES_ENDPOINT: "/devices",
+  ACTUATOR_COMMAND_ENDPOINT: "/devices/{deviceId}/command",
+  LOGS_ENDPOINT: "/logs",
 
-  PUMP_SETTING_ENDPOINT: "/settings/pump/",
-  MQTT_STATUS_ENDPOINT: "/mqtt/status/",
+  PUMP_SETTING_ENDPOINT: "/settings/pump",
+  MQTT_STATUS_ENDPOINT: "/mqtt/status",
 
   MQTT_DEFAULT: {
     broker: "xxxx.s1.eu.hivemq.cloud",
